0f9cd7b4227e105a5cbeac61dce246067ac49037
Dr. Ernesto Lee <socrates73@gmail.com>
2023-06-26 07:38:04 -0700

    Update 02b_llama_chroma.py

--- 02b_llama_chroma.py
+++ 02b_llama_chroma.py
@@ -9,18 +9,18 @@ load_dotenv()
 chroma_client = chromadb.Client()
 
 # create a collection
-chroma_collection = chroma_client.create_collection("newspieces")
+chroma_collection = chroma_client.create_collection("dlchapters")
 # https://docs.trychroma.com/api-reference
 print(chroma_collection.count())
 
-documents = SimpleDirectoryReader('news').load_data()
+documents = SimpleDirectoryReader('data').load_data()
 
 index = GPTChromaIndex.from_documents(documents, chroma_collection=chroma_collection)
 print(chroma_collection.count())
 print(chroma_collection.get()['documents'])
 print(chroma_collection.get()['metadatas'])
 
-index.save_to_disk("newspieces.json")
+index.save_to_disk("dlchapters.json")
 
 # During query time, the index uses Chroma to query for the top k
 # most similar nodes, and synthesizes an answer from the retrieved nodes.
